7337600ff534dd97fbbec0d8e5c35504b2d37e51
kamalyzl <kamaly.kazal@gmail.com>
2018-02-08 00:52:44 -0500

    readme completo

--- README.md
+++ README.md
@@ -1,11 +1,28 @@
 # Insta Collage
 
 
-### Instrucciones:
-* Proyecto desarrollado en vista mobile.
-* Usamos la **API Drag and Drop de HTML5** para crear el collage arrastrando y soltando las imagenes.
-* Las imágenes fueron de libre elección.
-* El mínimo de fotos para elegirr fueron de 6 y para el collage mínimo 3.
+## Descripción
+
+
+
+El reto de este proyecto es poder aplicar nuestro conocimiento en HTML 5, JavaScript ES6 y CSS, teniendo como condición las siguientes reglas:
+
+* Visualizarse en vista mobile.
+* Usar **API Drag and Drop de HTML5** para crear el collage arrastrando y soltando las imagenes.
+* Libre elección en imágenes. 
+ 
+
+## Herramientas utilizadas 
+ 1.  **HTML 5** : Estructura de la página.
+ 2. **JavaScript ES6** : Lenguaje de programación.
+ 3. **ED GRID**, framework CSS, nos sirvió para el Layaout y maquetado de la página en su versión Mobile.
+
+## Resultado
+
+![Resultado](assets/images/picture.png)
+
+
+
 
 
 ### Colaboradores
--- js/app.js
+++ js/app.js
@@ -6,13 +6,13 @@ let boxDestin = document.getElementById('content-box');
 let boxOrigin = document.getElementById('content');
 
 // Recoremos las etiquetas img
-images.forEach(function (value) {
-    value.addEventListener('dragstart', originStar);
-})
+images.forEach(function(value) {
+  value.addEventListener('dragstart', originStar);
+});
 
-boxDestin.addEventListener('dragover', function (e) {
-    e.preventDefault();
-})
+boxDestin.addEventListener('dragover', function(e) {
+  e.preventDefault();
+});
 
 boxDestin.addEventListener('drop', destinFin);
 boxOrigin.addEventListener('dragend', finished); 
@@ -21,31 +21,31 @@ boxDestin.addEventListener('dragleave', initLeave);
 
 // Funciones globales 
 function originStar(e) {
-    let element = e.target;
-    e.dataTransfer.setData('text', element.getAttribute('id'));
+  let element = e.target;
+  e.dataTransfer.setData('text', element.getAttribute('id'));
 }
 
 function destinFin(e) {
-    e.preventDefault();
-    let id = e.dataTransfer.getData('text');
-    let src = document.getElementById(id).src;
-// Contenido img en el nuestro contenedor
-    boxDestin.innerHTML += `  <img class="one" src="${src}" alt="${src}">`;
+  e.preventDefault();
+  let id = e.dataTransfer.getData('text');
+  let src = document.getElementById(id).src;
+  // Contenido img en el nuestro contenedor
+  boxDestin.innerHTML += `  <img class="one" src="${src}" alt="${src}">`;
 }
 
 // Le decimos que hacer al soltar el cursor en el contenedor de origen
 function finished(e) {
-    let element = e.target;    
-    // element.style.visibility = 'hidden';
+  let element = e.target;    
+  // element.style.visibility = 'hidden';
 }
 // al solo ingresar al nuevo contenedor, hacemos que cambien de fondo
 function initGo(e) {
-    e.preventDefault();
-    // boxDestin.style.background = '#5B4E77';
-    boxDestin.style.border = '1px solid #55D6BE';
+  e.preventDefault();
+  // boxDestin.style.background = '#5B4E77';
+  boxDestin.style.border = '1px solid #55D6BE';
 }
 // al salir del nuevo contenedor, hacemos que el fondo desaparezca
 function initLeave(e) {
-    e.preventDefault();
-    boxDestin.style.background = 'none';
+  e.preventDefault();
+  boxDestin.style.background = 'none';
 }
